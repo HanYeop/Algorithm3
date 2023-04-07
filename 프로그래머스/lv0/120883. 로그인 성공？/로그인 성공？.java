0c616fe04ff0b7d7efd65b16f8989f1f36dcaa60
Sangyeop Han <50766393+HanYeop@users.noreply.github.com>
2023-04-07 13:35:44 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        
+        Map<String, String> memberTable = new HashMap<>();
+
+		for (int i = 0; i < db.length; i++) {
+			memberTable.put(db[i][0], db[i][1]);
+		}
+		try {
+			if (memberTable.get(id_pw[0]).equals(id_pw[1])) {
+				return "login";
+			} else if (!memberTable.get(id_pw[0]).equals(id_pw[1])) {
+				return "wrong pw";
+			} else {
+				return "fail";
+			}
+		} catch (Exception e) {
+			return "fail";
+		}
+        
+    }
+}
