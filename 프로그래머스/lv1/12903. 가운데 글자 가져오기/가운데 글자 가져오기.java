f27ebabff385bb9c34359e0597ddf088ce69c804
Sangyeop Han <50766393+HanYeop@users.noreply.github.com>
2023-04-27 20:10:09 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        int len = s.length();
+        
+        if(len %2 == 0){
+            sb.append(s.charAt(len / 2 - 1));
+        }
+        sb.append(s.charAt(len / 2));
+
+        answer = sb.toString();  
+
+        return answer;
+    }
+}
